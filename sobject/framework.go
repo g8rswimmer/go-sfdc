fb7b32404d63f41ec1a18d4a44a6e5ecb84dd4f4
seanjustice <sean.justice@soleranetwork.com>
2019-04-05 21:05:44 -0700

    removed comments

--- sobject/framework.go
+++ sobject/framework.go
@@ -10,44 +10,6 @@ import (
 	"github.com/g8rswimmer/goforce/session"
 )
 
-// Framework is the Salesforce SObject API framework.
-//
-// Metadata will return a SObject's metadata.
-//
-// Describe will return a SObject's descibe
-//
-// Insert will insert a record into the Salesforce org.
-//
-// Update will update a record into the Salesforce org.
-//
-// Upsert will upsert (update or insert) a record inot the Salesforce org.
-// The external id, defined in the record, can be used as an ID.
-//
-// Delete will remove the record into the Salesforce org.
-//
-// Get will retrieve an objects record from the id or external id.
-//
-// AttachmentBody will retrieve the attachment's blob.
-//
-// DocumentBody will retrieve the document's blob.
-//
-// DeletedRecords will return the delete records from a date range.
-//
-// UpdatedRecords will return the updated records from a date range.
-type Framework interface {
-	// Metadata(string) (MetadataValue, error)
-	// Describe(string) (DescribeValue, error)
-	// Insert(Inserter) (InsertValue, error)
-	// Update(*goforce.Record) error
-	// Upsert(*goforce.Record) (UpdateValue, error)
-	// Delete(*goforce.Record) (DeleteValue, error)
-	// Get(*goforce.Record) error
-	// AttachmentBody(string) (AttachmentBody, error)
-	// DocumentBody(string) (DocumentBody, error)
-	// DeletedRecords(string, time.Time, time.Time) ([]DeleteValue, error)
-	// UpdatedRecords(string, time.Time, time.Time) ([]UpdateValue, error)
-}
-
 // ObjectURLs is the URL for the SObject metadata.
 type ObjectURLs struct {
 	CompactLayouts   string `json:"compactLayouts"`
