c7e3f0569100f56b663ecab8638f1949cd58d3b7
seanjustice <sean.justice@soleranetwork.com>
2019-04-08 20:47:29 -0700

    Rounded out builder Added unit tests

--- soql/builder.go
+++ soql/builder.go
@@ -1,6 +1,7 @@
 package soql
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -9,47 +10,87 @@ import (
 type Builder struct {
 	fieldList  []string
 	objectType string
-	subQuery   []fmt.Stringer
-	where      fmt.Stringer
-	order      fmt.Stringer
+	subQuery   []Querier
+	where      WhereClauser
+	order      Orderer
 	limit      int
 	offset     int
 }
 
+type Querier interface {
+	Query() (string, error)
+}
+
+func NewBuilder(object string) (*Builder, error) {
+	if object == "" {
+		return nil, errors.New("builder: object type can not be an empty string")
+	}
+	return &Builder{
+		objectType: object,
+	}, nil
+}
 func (b *Builder) FieldList(fields ...string) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.fieldList = append(b.fieldList, fields...)
 }
-func (b *Builder) ObjectType(object string) {
-	b.objectType = object
-}
-func (b *Builder) SubQuery(query fmt.Stringer) {
+func (b *Builder) SubQuery(query Querier) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.subQuery = append(b.subQuery, query)
 }
-func (b *Builder) Where(where fmt.Stringer) {
+func (b *Builder) Where(where WhereClauser) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.where = where
 }
-func (b *Builder) OrderBy(order fmt.Stringer) {
+func (b *Builder) OrderBy(order Orderer) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.order = order
 }
 func (b *Builder) Limit(limit int) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.limit = limit
 }
 func (b *Builder) Offset(offset int) {
+	if b == nil {
+		panic("builder can not be nil")
+	}
 	b.offset = offset
 }
-func (b *Builder) String() string {
+func (b *Builder) Query() (string, error) {
+	if b.objectType == "" {
+		return "", errors.New("builder: object type can not be an empty string")
+	}
+	if len(b.fieldList) == 0 {
+		return "", errors.New("builder: field list must be have fields present")
+	}
+
 	soql := "SELECT " + strings.Join(b.fieldList, ",")
 	if b.subQuery != nil {
 		for _, query := range b.subQuery {
-			soql += fmt.Sprintf(",(%s)", query.String())
+			var sub string
+			var err error
+			if sub, err = query.Query(); err == nil {
+				soql += fmt.Sprintf(",(%s)", sub)
+			} else {
+				return "", err
+			}
 		}
 	}
 	soql += " FROM " + b.objectType
 	if b.where != nil {
-		soql += " WHERE " + b.where.String()
+		soql += " " + b.where.Clause()
 	}
 	if b.order != nil {
-		soql += " ORDER BY " + b.order.String()
+		soql += " " + b.order.Order()
 	}
 	if b.limit > 0 {
 		soql += fmt.Sprintf(" LIMIT %d", b.limit)
@@ -57,30 +98,39 @@ func (b *Builder) String() string {
 	if b.offset > 0 {
 		soql += fmt.Sprintf(" OFFSET %d", b.offset)
 	}
-	return soql
+	return soql, nil
 }
 
 type WhereClause struct {
-	clause string
+	expression string
 }
 
-func WhereLike(field string, value string) *WhereClause {
-	return &WhereClause{
-		clause: fmt.Sprintf("%s LIKE '%s'", field, value),
-	}
+type WhereExpression interface {
+	Expression() string
 }
-func WhereGreaterThan(field string, value interface{}, equals bool) *WhereClause {
+type WhereClauser interface {
+	Clause() string
+}
+
+func WhereLike(field string, value string) (*WhereClause, error) {
+	return &WhereClause{
+		expression: fmt.Sprintf("%s LIKE '%s'", field, value),
+	}, nil
+}
+func WhereGreaterThan(field string, value interface{}, equals bool) (*WhereClause, error) {
 	var v string
 	if value != nil {
 		switch value.(type) {
-		case string:
-			v = fmt.Sprintf("'%s'", value.(string))
+		case string, bool:
+			return nil, errors.New("where greater than: value can not be a string or bool")
 		case time.Time:
 			date := value.(time.Time)
 			v = date.Format(time.RFC3339)
 		default:
 			v = fmt.Sprintf("%v", value)
 		}
+	} else {
+		return nil, errors.New("where greater than: value can not be nil")
 	}
 
 	operator := ">"
@@ -89,21 +139,23 @@ func WhereGreaterThan(field string, value interface{}, equals bool) *WhereClause
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s %s %s", field, operator, v),
-	}
+		expression: fmt.Sprintf("%s %s %s", field, operator, v),
+	}, nil
 }
-func WhereLessThan(field string, value interface{}, equals bool) *WhereClause {
+func WhereLessThan(field string, value interface{}, equals bool) (*WhereClause, error) {
 	var v string
 	if value != nil {
 		switch value.(type) {
-		case string:
-			v = fmt.Sprintf("'%s'", value.(string))
+		case string, bool:
+			return nil, errors.New("where less than: value can not be a string")
 		case time.Time:
 			date := value.(time.Time)
 			v = date.Format(time.RFC3339)
 		default:
 			v = fmt.Sprintf("%v", value)
 		}
+	} else {
+		return nil, errors.New("where less than: value can not be nil")
 	}
 
 	operator := "<"
@@ -112,10 +164,10 @@ func WhereLessThan(field string, value interface{}, equals bool) *WhereClause {
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s %s %s", field, operator, v),
-	}
+		expression: fmt.Sprintf("%s %s %s", field, operator, v),
+	}, nil
 }
-func WhereEquals(field string, value interface{}) *WhereClause {
+func WhereEquals(field string, value interface{}) (*WhereClause, error) {
 	var v string
 	if value != nil {
 		switch value.(type) {
@@ -132,10 +184,10 @@ func WhereEquals(field string, value interface{}) *WhereClause {
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s = %s", field, v),
-	}
+		expression: fmt.Sprintf("%s = %s", field, v),
+	}, nil
 }
-func WhereNotEquals(field string, value interface{}) *WhereClause {
+func WhereNotEquals(field string, value interface{}) (*WhereClause, error) {
 	var v string
 	if value != nil {
 		switch value.(type) {
@@ -152,15 +204,17 @@ func WhereNotEquals(field string, value interface{}) *WhereClause {
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s != %s", field, v),
-	}
+		expression: fmt.Sprintf("%s != %s", field, v),
+	}, nil
 }
-func WhereIn(field string, values []interface{}) *WhereClause {
+func WhereIn(field string, values []interface{}) (*WhereClause, error) {
 	set := make([]string, len(values))
 	for idx, value := range values {
 		switch value.(type) {
 		case string:
 			set[idx] = fmt.Sprintf("'%s'", value.(string))
+		case bool:
+			return nil, errors.New("where in: boolean is not a value set value")
 		case time.Time:
 			date := value.(time.Time)
 			set[idx] = date.Format(time.RFC3339)
@@ -170,22 +224,129 @@ func WhereIn(field string, values []interface{}) *WhereClause {
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s IN (%s)", field, strings.Join(set, ",")),
-	}
+		expression: fmt.Sprintf("%s IN (%s)", field, strings.Join(set, ",")),
+	}, nil
 }
 
-func WhereNotIn(field string, values []interface{}) *WhereClause {
+func WhereNotIn(field string, values []interface{}) (*WhereClause, error) {
 	set := make([]string, len(values))
 	for idx, value := range values {
 		switch value.(type) {
 		case string:
 			set[idx] = fmt.Sprintf("'%s'", value.(string))
+		case bool:
+			return nil, errors.New("where not in: boolean is not a value set value")
+		case time.Time:
+			date := value.(time.Time)
+			set[idx] = date.Format(time.RFC3339)
 		default:
 			set[idx] = fmt.Sprintf("%v", value)
 		}
 	}
 
 	return &WhereClause{
-		clause: fmt.Sprintf("%s NOT IN (%s)", field, strings.Join(set, ",")),
+		expression: fmt.Sprintf("%s NOT IN (%s)", field, strings.Join(set, ",")),
+	}, nil
+}
+
+func (wc *WhereClause) Clause() string {
+	if wc == nil {
+		panic("WhereClause can not be nil")
 	}
+
+	return fmt.Sprintf("WHERE %s", wc.expression)
+}
+func (wc *WhereClause) Group() {
+	if wc == nil {
+		panic("WhereClause can not be nil")
+	}
+	wc.expression = fmt.Sprintf("(%s)", wc.expression)
+}
+func (wc *WhereClause) And(where WhereExpression) {
+	if wc == nil {
+		panic("WhereClause can not be nil")
+	}
+	wc.expression = fmt.Sprintf("%s AND %s", wc.expression, where.Expression())
+}
+func (wc *WhereClause) Or(where WhereExpression) {
+	if wc == nil {
+		panic("WhereClause can not be nil")
+	}
+	wc.expression = fmt.Sprintf("%s OR %s", wc.expression, where.Expression())
+}
+func (wc *WhereClause) Expression() string {
+	if wc == nil {
+		panic("WhereClause can not be nil")
+	}
+	return wc.expression
+}
+
+type OrderResult string
+
+const (
+	OrderAsc  OrderResult = "ASC"
+	OrderDesc OrderResult = "DESC"
+)
+
+type OrderNulls string
+
+const (
+	OrderNullsLast  OrderNulls = "NULLS LAST"
+	OrderNullsFirst OrderNulls = "NULLS FIRST"
+)
+
+type OrderBy struct {
+	fieldOrder []string
+	result     OrderResult
+	nulls      OrderNulls
+}
+
+type Orderer interface {
+	Order() string
+}
+
+func NewOrderBy(result OrderResult) (*OrderBy, error) {
+	switch result {
+	case OrderAsc, OrderDesc:
+	default:
+		return nil, fmt.Errorf("order by: %s is not a valid result ordering type", string(result))
+	}
+
+	return &OrderBy{
+		result: result,
+	}, nil
+
+}
+func (o *OrderBy) FieldOrder(fields ...string) {
+	if o == nil {
+		panic("OrderBy can not be nil")
+	}
+
+	o.fieldOrder = append(o.fieldOrder, fields...)
+}
+
+func (o *OrderBy) NullOrdering(nulls OrderNulls) error {
+	if o == nil {
+		panic("OrderBy can not be nil")
+	}
+
+	switch nulls {
+	case OrderNullsLast, OrderNullsFirst:
+	default:
+		return fmt.Errorf("order by: %s is not a valid null ordering type", string(nulls))
+	}
+	o.nulls = nulls
+	return nil
+}
+
+func (o *OrderBy) Order() string {
+	if o == nil {
+		panic("OrderBy can not be nil")
+	}
+
+	orderBy := "ORDER BY " + strings.Join(o.fieldOrder, ",") + " " + string(o.result)
+	if o.nulls != "" {
+		orderBy += " " + string(o.nulls)
+	}
+	return orderBy
 }
