e06923eb7da1d5ed33a694442f7ac859cea25fd5
seanjustice <sean.justice@soleranetwork.com>
2019-04-11 20:54:51 -0700

    completed the unit tests

--- soql/result_test.go
+++ soql/result_test.go
@@ -1,7 +1,10 @@
 package soql
 
 import (
+	"io/ioutil"
+	"net/http"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -268,6 +271,139 @@ func TestQueryResult_Records(t *testing.T) {
 			}
 			if got := result.Records(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("QueryResult.Records() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestQueryResult_Next(t *testing.T) {
+	type fields struct {
+		response queryResponse
+		records  []*QueryRecord
+		resource *Resource
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    *QueryResult
+		wantErr bool
+	}{
+		{
+			name:    "No more records",
+			fields:  fields{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "No more records",
+			fields: fields{
+				response: queryResponse{
+					NextRecordsURL: "/services/data/v20.0/query/01gD0000002HU6KIAW-2000",
+				},
+				resource: &Resource{
+					session: &mockSessionFormatter{
+						url: "https://test.salesforce.com",
+						client: mockHTTPClient(func(req *http.Request) *http.Response {
+							if req.URL.String() != "https://test.salesforce.com/services/data/v20.0/query/01gD0000002HU6KIAW-2000" {
+								return &http.Response{
+									StatusCode: 500,
+									Status:     "Some Status",
+									Body:       ioutil.NopCloser(strings.NewReader("Error")),
+									Header:     make(http.Header),
+								}
+							}
+							resp := `
+							{
+								"done" : true,
+								"totalSize" : 2,
+								"records" : 
+								[ 
+									{  
+										"attributes" : 
+										{    
+											"type" : "Account",    
+											"url" : "/services/data/v20.0/sobjects/Account/001D000000IRFmaIAH"  
+										},  
+										"Name" : "Test 1"
+									}, 
+									{  
+										"attributes" : 
+										{    
+											"type" : "Account",    
+											"url" : "/services/data/v20.0/sobjects/Account/001D000000IomazIAB"  
+										},  
+										"Name" : "Test 2"
+									}
+								]
+							}`
+
+							return &http.Response{
+								StatusCode: 200,
+								Body:       ioutil.NopCloser(strings.NewReader(resp)),
+								Header:     make(http.Header),
+							}
+						}),
+					},
+				},
+			},
+			want: &QueryResult{
+				response: queryResponse{
+					Done:      true,
+					TotalSize: 2,
+					Records: []map[string]interface{}{
+						{
+							"attributes": map[string]interface{}{
+								"type": "Account",
+								"url":  "/services/data/v20.0/sobjects/Account/001D000000IRFmaIAH",
+							},
+							"Name": "Test 1",
+						},
+						{
+							"attributes": map[string]interface{}{
+								"type": "Account",
+								"url":  "/services/data/v20.0/sobjects/Account/001D000000IomazIAB",
+							},
+							"Name": "Test 2",
+						},
+					},
+				},
+				records: testNewQueryRecords([]map[string]interface{}{
+					{
+						"attributes": map[string]interface{}{
+							"type": "Account",
+							"url":  "/services/data/v20.0/sobjects/Account/001D000000IRFmaIAH",
+						},
+						"Name": "Test 1",
+					},
+					{
+						"attributes": map[string]interface{}{
+							"type": "Account",
+							"url":  "/services/data/v20.0/sobjects/Account/001D000000IomazIAB",
+						},
+						"Name": "Test 2",
+					},
+				}),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := &QueryResult{
+				response: tt.fields.response,
+				records:  tt.fields.records,
+				resource: tt.fields.resource,
+			}
+			got, err := result.Next()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryResult.Next() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.want != nil {
+				tt.want.resource = result.resource
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("QueryResult.Next() = %v, want %v", got, tt.want)
 			}
 		})
 	}
