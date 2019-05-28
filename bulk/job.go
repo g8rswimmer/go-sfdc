c284e0d442eea3009faba5afeabf6b6389974efa
seanjustice <sean.justice@soleranetwork.com>
2019-05-27 20:43:43 -0700

    working on the job

--- bulk/job.go
+++ bulk/job.go
@@ -1,11 +1,15 @@
 package bulk
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	sfdc "github.com/g8rswimmer/go-sfdc"
 	"github.com/g8rswimmer/go-sfdc/session"
@@ -62,6 +66,21 @@ const (
 
 const createEndpoint = "/jobs/ingest"
 
+type JobUnprocessedRecord struct {
+	Fields map[string]string
+}
+type JobRecord struct {
+	ID string
+	JobUnprocessedRecord
+}
+type JobSuccessfulRecord struct {
+	Created bool
+	JobRecord
+}
+type JobFailedRecord struct {
+	Error string
+	JobRecord
+}
 type JobOptions struct {
 	ColumnDelimiter     ColumnDelimiter `json:"columnDelimiter"`
 	ContentType         ContentType     `json:"contentType"`
@@ -280,4 +299,215 @@ func (j *Job) Delete() error {
 		return errors.New("job error: unable to delete job")
 	}
 	return nil
+}
+
+func (j *Job) Upload(body io.Reader) error {
+	url := j.session.ServiceURL() + createEndpoint + "/" + j.info.ID + "/batches"
+	request, err := http.NewRequest(http.MethodPut, url, body)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "text/csv")
+	j.session.AuthorizationHeader(request)
+
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusCreated {
+		return errors.New("job error: unable to upload job")
+	}
+	return nil
+}
+
+func (j *Job) SuccessfulRecords() ([]JobSuccessfulRecord, error) {
+	url := j.session.ServiceURL() + createEndpoint + "/" + j.info.ID + "/successfulResults/"
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Accept", "text/csv")
+	j.session.AuthorizationHeader(request)
+
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		decoder := json.NewDecoder(response.Body)
+		defer response.Body.Close()
+		var errs []sfdc.Error
+		err = decoder.Decode(&errs)
+		var errMsg error
+		if err == nil {
+			for _, err := range errs {
+				errMsg = fmt.Errorf("job err: %s: %s", err.ErrorCode, err.Message)
+			}
+		} else {
+			errMsg = fmt.Errorf("job err: %d %s", response.StatusCode, response.Status)
+		}
+		return nil, errMsg
+	}
+
+	scanner := bufio.NewScanner(response.Body)
+	defer response.Body.Close()
+	scanner.Split(bufio.ScanLines)
+	var records []JobSuccessfulRecord
+	delimiter := j.delimiter()
+	fields, err := j.fields(scanner, delimiter, 2)
+	if err != nil {
+		return nil, err
+	}
+	for scanner.Scan() {
+		var record JobSuccessfulRecord
+		values := strings.Split(scanner.Text(), delimiter)
+		created, err := strconv.ParseBool(values[0])
+		if err != nil {
+			return nil, err
+		}
+		record.Created = created
+		record.ID = values[1]
+		record.Fields = j.record(fields, values[2:])
+		records = append(records, record)
+	}
+
+	return records, nil
+}
+
+func (j *Job) FailedRecords() ([]JobFailedRecord, error) {
+	url := j.session.ServiceURL() + createEndpoint + "/" + j.info.ID + "/failedResults/"
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Accept", "text/csv")
+	j.session.AuthorizationHeader(request)
+
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		decoder := json.NewDecoder(response.Body)
+		defer response.Body.Close()
+		var errs []sfdc.Error
+		err = decoder.Decode(&errs)
+		var errMsg error
+		if err == nil {
+			for _, err := range errs {
+				errMsg = fmt.Errorf("job err: %s: %s", err.ErrorCode, err.Message)
+			}
+		} else {
+			errMsg = fmt.Errorf("job err: %d %s", response.StatusCode, response.Status)
+		}
+		return nil, errMsg
+	}
+
+	scanner := bufio.NewScanner(response.Body)
+	defer response.Body.Close()
+	scanner.Split(bufio.ScanLines)
+	var records []JobFailedRecord
+	delimiter := j.delimiter()
+	fields, err := j.fields(scanner, delimiter, 2)
+	if err != nil {
+		return nil, err
+	}
+	for scanner.Scan() {
+		var record JobFailedRecord
+		values := strings.Split(scanner.Text(), delimiter)
+		record.Error = values[0]
+		record.ID = values[1]
+		record.Fields = j.record(fields, values[2:])
+		records = append(records, record)
+	}
+
+	return records, nil
+}
+
+func (j *Job) UnprocessedRecords() ([]JobUnprocessedRecord, error) {
+	url := j.session.ServiceURL() + createEndpoint + "/" + j.info.ID + "/failedResults/"
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Accept", "text/csv")
+	j.session.AuthorizationHeader(request)
+
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		decoder := json.NewDecoder(response.Body)
+		defer response.Body.Close()
+		var errs []sfdc.Error
+		err = decoder.Decode(&errs)
+		var errMsg error
+		if err == nil {
+			for _, err := range errs {
+				errMsg = fmt.Errorf("job err: %s: %s", err.ErrorCode, err.Message)
+			}
+		} else {
+			errMsg = fmt.Errorf("job err: %d %s", response.StatusCode, response.Status)
+		}
+		return nil, errMsg
+	}
+
+	scanner := bufio.NewScanner(response.Body)
+	defer response.Body.Close()
+	scanner.Split(bufio.ScanLines)
+	var records []JobUnprocessedRecord
+	delimiter := j.delimiter()
+	fields, err := j.fields(scanner, delimiter, 0)
+	if err != nil {
+		return nil, err
+	}
+	for scanner.Scan() {
+		var record JobUnprocessedRecord
+		values := strings.Split(scanner.Text(), delimiter)
+		record.Fields = j.record(fields, values)
+		records = append(records, record)
+	}
+
+	return records, nil
+}
+
+func (j *Job) fields(scanner *bufio.Scanner, delimiter string, offset int) ([]string, error) {
+	if scanner.Scan() == false {
+		return nil, errors.New("job: response needs to have header")
+	}
+	values := strings.Split(scanner.Text(), delimiter)
+	fields := make([]string, len(values)-offset)
+	copy(values[offset:], fields[:])
+	return fields, nil
+}
+func (j *Job) record(fields, values []string) map[string]string {
+	record := make(map[string]string)
+	for idx, field := range fields {
+		record[field] = values[idx]
+	}
+	return record
+}
+
+func (j *Job) delimiter() string {
+	switch ColumnDelimiter(j.info.ColumnDelimiter) {
+	case Tab:
+		return "\t"
+	case SemiColon:
+		return ";"
+	case Pipe:
+		return "|"
+	case Caret:
+		return "^"
+	case Backquote:
+		return "`"
+	case Comma:
+		fallthrough
+	default:
+		return ","
+	}
 }
