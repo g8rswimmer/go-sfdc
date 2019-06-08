a4c60dd1f8b2cd107bd3d2bc5d02c6a570fba272
seanjustice <sean.justice@soleranetwork.com>
2019-06-07 19:14:35 -0700

    Created the formatter tests

--- bulk/formatter.go
+++ bulk/formatter.go
@@ -6,18 +6,21 @@ import (
 	"strings"
 )
 
+// Record is the interface to the fields of the bulk uploader record.
 type Record interface {
 	Fields() map[string]interface{}
+	InsertNull() bool
 }
 
+// Formatter is the object that will add records for the bulk uploader.
 type Formatter struct {
-	job        *Job
-	fields     []string
-	insertNull bool
-	sb         strings.Builder
+	job    *Job
+	fields []string
+	sb     strings.Builder
 }
 
-func NewFormatter(job *Job, fields []string, insertNull bool) (*Formatter, error) {
+// NewFormatter creates a new formatter using the job and the list of fields.
+func NewFormatter(job *Job, fields []string) (*Formatter, error) {
 	if job == nil {
 		return nil, errors.New("bulk formatter: job is required for the formatter")
 	}
@@ -26,17 +29,23 @@ func NewFormatter(job *Job, fields []string, insertNull bool) (*Formatter, error
 	}
 
 	f := &Formatter{
-		job:        job,
-		fields:     fields,
-		insertNull: insertNull,
-		sb:         strings.Builder{},
+		job:    job,
+		fields: fields,
+		sb:     strings.Builder{},
 	}
-	f.sb.WriteString(strings.Join(fields, job.delimiter()))
-	f.sb.WriteString(job.newline())
+	_, err := f.sb.WriteString(strings.Join(fields, job.delimiter()))
+	if err != nil {
+		return nil, err
+	}
+	_, err = f.sb.WriteString(job.newline())
+	if err != nil {
+		return nil, err
+	}
 
 	return f, nil
 }
 
+// Add will place a record in the bulk uploader.
 func (f *Formatter) Add(records ...Record) error {
 	if records == nil {
 		return errors.New("bulk formatter: record interface can not be nil")
@@ -45,8 +54,9 @@ func (f *Formatter) Add(records ...Record) error {
 	for _, record := range records {
 		recFields := record.Fields()
 		values := make([]string, len(f.fields))
+		insertNull := record.InsertNull()
 		for idx, field := range f.fields {
-			if f.insertNull {
+			if insertNull {
 				values[idx] = "#N/A"
 			} else {
 				values[idx] = ""
@@ -57,14 +67,21 @@ func (f *Formatter) Add(records ...Record) error {
 				}
 			}
 		}
-		f.sb.WriteString(strings.Join(values, f.job.delimiter()))
-		f.sb.WriteString(f.job.newline())
+		_, err := f.sb.WriteString(strings.Join(values, f.job.delimiter()))
+		if err != nil {
+			return err
+		}
+		_, err = f.sb.WriteString(f.job.newline())
+		if err != nil {
+			return err
+		}
 
 	}
 
 	return nil
 }
 
+// Reader will return a reader of the bulk uploader field record body.
 func (f *Formatter) Reader() *strings.Reader {
 	return strings.NewReader(f.sb.String())
 }
