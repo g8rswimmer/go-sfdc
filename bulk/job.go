81614a9cca2dc865cfa5673b095334c188960eb1
Rener <>
2019-09-13 19:16:43 -0300

    fix

--- bulk/job.go
+++ bulk/job.go
@@ -222,7 +222,7 @@ func (j *Job) formatOptions() error {
 			return errors.New("bulk job: external id field name is required for upsert operation")
 		}
 	}
-	if j.options.Object == "" && (j.options.Operation != "query" || j.options.Operation != "queryAll") {
+	if j.options.Object == "" && (j.options.Operation != "query" && j.options.Operation != "queryAll") {
 		return errors.New("bulk job: object is required")
 	}
 	if j.options.LineEnding == "" {
