6b9708afaa4ce5da2c6013aafd0fbfbc5bc7c4d8
Yandry Pozo <yandry.pozo-castillo@jumpcloud.com>
2019-11-20 11:38:00 -0700

    Keep original imports

--- bulk/job.go
+++ bulk/job.go
@@ -11,8 +11,8 @@ import (
 	"strconv"
 	"strings"
 
-	sfdc "github.com/TheJumpCloud/go-sfdc"
-	"github.com/TheJumpCloud/go-sfdc/session"
+	sfdc "github.com/g8rswimmer/go-sfdc"
+	"github.com/g8rswimmer/go-sfdc/session"
 )
 
 // JobType is the bulk job type.
